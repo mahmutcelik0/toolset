4b2448a0256a731ec814acb0f862fd9b15286cbe
mahmutcelik01 <mahmut.celikcelik1@gmail.com>
2026-06-23 21:58:09 +0300

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer"
 import CommandPalette from "@/components/CommandPalette"
 import "./globals.css"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({ subsets: ["latin"], variable: "--font-geist-sans" })
 const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-geist-mono" })
@@ -28,6 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Footer />
         <CommandPalette />
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   )
